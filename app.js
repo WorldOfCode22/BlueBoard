27255c93adb21742f53a3aefbd629b8970a87d1f
Sloan <gwaltneys2@winthrop.edu>
2018-01-28 23:05:21 -0500

    added nodemon and basic database connection

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const port = 3000;
+const keys = require("./config/keys");
+const mongoose = require("mongoose");
+mongoose.connect(keys.database.URI).then(()=>{
+    console.log("Database Connected");
+},
+err =>{console.log("Datbase connection error: " + err)});
 var app = express();
 
 app.listen(port,()=>{
